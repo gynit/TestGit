2d1919789cd2610c212d052ca7f3017bb2b54c38
tom_glb <goodMorning_glb@sansi.top>
2025-04-10 19:59:31 +0800

    edit on branch hot_fix2

--- src/main/java/com/sansi/Happy.java
+++ src/main/java/com/sansi/Happy.java
@@ -6,5 +6,6 @@ public class Happy {
         System.out.println("left2 ..");
         System.out.println("right ..");
         System.out.println("hot fix ...");
+        System.out.println("hot fix2 ...");
     }
 }
